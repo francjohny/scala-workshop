ea20e0c67d02e412c9dcb66bbc3cf6ceb9c4ff87
Francis Johny <francjohny@gmail.com>
2018-01-12 01:10:39 +0530

    Add a solution to compare the triplets
    https://www.hackerrank.com/challenges/compare-the-triplets/problem

---
+++ src/main/scala/week1/pitfalls/Solution.scala
@@ -0,0 +1,23 @@
+package week1.pitfalls
+
+/**
+  * Sample Input
+  * 5 6 7
+  * 3 6 10
+  *
+  * Sample Output
+  * 1 1
+  */
+
+object Solution {
+
+  def main(args: Array[String]): Unit = {
+    val ints: List[Array[Int]] = io.Source.stdin.getLines().take(2).toList.map(_.split("[ ]").map(_.toInt))
+    val a: Array[Int] = ints.head
+    val b: Array[Int] = ints.last
+    val differentScores = a.zip(b).filter(x ⇒ x._1 != x._2)
+    val aliceScore = differentScores.count(x ⇒ x._1 > x._2)
+    val bobScore = differentScores.length - aliceScore
+    println(s"$aliceScore $bobScore")
+  }
+}
